08d6834f1c3466afb7c24c62eaa7ef37ade1e6e9
rohan1reckitt <rohan.kumar1@reckitt.com>
2025-04-10 15:22:33 +0530

    Update command

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const API_BASE_URL = "http://127.0.0.1:5000/get_data";
+const API_BASE_URL = "https://daily-check-khaki.vercel.app/get_data";
 
 const countries = {
   me: "Middle East",
